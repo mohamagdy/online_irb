464793b13d905539f0ad6b3260f67329abe8a351
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-03-28 19:47:39 +0200

    First commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,12 @@
 class HomeController < ApplicationController
   def index
-    session[:binding] = Output.new.send(:binding).to_msgpack 
   end
   
   def command
    begin
-    render :text => eval(%Q{#{params[:command]}}, MessagePack.unpack(session[:binding]))
+    render :text => eval(%Q{#{params[:command]}}, TOPLEVEL_BINDING)
    rescue Exception => e
     render :text => e
    end
   end
 end
-
-    
-z=1
-x=20
-y=x+z
-(1..3).each do |t|
-  p t
-end
-puts "yes"
